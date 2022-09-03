07488fc17211c0511be7ffcd0a3b82b904ed35c4
JohnnyOhms <officialjohn662@gmail.com>
2022-09-03 22:23:54 +0100

    uploaded

---
+++ src/functions.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+// functions
+function returnData(code) {
+    return 0;
+}
+function userInfo(info) {
+    var user = info;
+    if (user > 10)
+        return info = info * 5;
+    return info * 10;
+}
