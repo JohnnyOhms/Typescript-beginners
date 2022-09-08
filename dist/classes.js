9a10251ee555a3387cd38c2f873cee3acc4f06a6
JohnnyOhms <officialjohn662@gmail.com>
2022-09-08 20:00:59 +0100

    uploaded

--- dist/classes.js
+++ dist/classes.js
@@ -8,11 +8,48 @@ const userInfo = {
 };
 class USerNames {
     constructor(name, id, data) {
+        this.orders = ["Beans"];
         this.name = name;
         this.id = id;
         this.data = data;
     }
+    addNameToOrder(surname) {
+        this.orders.push(this.name);
+        this.orders = [...this.orders, surname];
+        console.log(this.orders);
+    }
+    getOrder(order) {
+        this.orders.push(order);
+        console.log(this.orders);
+    }
 }
-const userNames = new USerNames("JohnnyOhms", 247, userInfo);
-console.log(userNames);
+class MainName extends USerNames {
+    constructor(data) {
+        super("John", 22, userInfo);
+        let datainfo = data;
+        this.dataList(datainfo);
+    }
+    dataList(data) {
+        console.log(data);
+    }
+}
+const main = new MainName(55);
+class Surname extends MainName {
+    constructor(middleName, surname) {
+        super(65);
+        this.middleName = middleName;
+        this.surname = surname;
+    }
+    addSurname(surName) {
+        this.surname.push(surName);
+        this.surname.push(this.middleName);
+    }
+    printSurname() {
+        console.log(this.surname);
+    }
+}
+const surname = new Surname("Ohms", []);
+surname.addSurname("John");
+surname.dataList(55);
+surname.printSurname();
 //# sourceMappingURL=classes.js.map
--- src/classes.ts
+++ src/classes.ts
@@ -16,14 +16,65 @@ class USerNames{
     name: string
     id: number
     data: Format
+    orders: string[] = ["Beans"]
 
-    constructor(name: string, id: number, data: Format ) {
+    constructor( name: string, id: number, data: Format ) {
         this.name = name
         this.id = id
         this.data = data
     }
+
+    addNameToOrder(surname: string): void {
+        this.orders.push( this.name )
+        this.orders = [...this.orders, surname]
+        console.log(this.orders);
+        
+    }
+
+    getOrder(order: string): void {
+        this.orders.push( order )
+        console.log(this.orders);
+        
+    }
 }
 
-const userNames = new USerNames( "JohnnyOhms", 247, userInfo )
+// const userNames = new USerNames( "JohnnyOhms", 247, userInfo )
+// userNames.getOrder("rice")
+// userNames.addNameToOrder("Ohms")
 
-console.log(userNames);
+
+class MainName extends USerNames {
+    constructor(data: number) {
+        super( "John", 22, userInfo )
+        let datainfo: number = data;
+        this.dataList(datainfo)
+    }
+
+    dataList(data: number) {
+        console.log(data);
+        
+    }
+}
+
+const main = new MainName(55)
+
+class Surname extends MainName{
+    constructor( public middleName: string, public surname: string[] ) {
+        super(65)
+    }
+
+    addSurname(surName: string) {
+        this.surname.push(surName)
+        this.surname.push(this.middleName)
+    }
+
+    printSurname(): void {
+        console.log(this.surname);
+        
+    }
+}
+
+const surname = new Surname( "Ohms", [] )
+surname.addSurname( "John" )
+surname.dataList(55)
+surname.printSurname()
